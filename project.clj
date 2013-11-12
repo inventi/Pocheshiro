c0de029b5398aa9d166524be1aea9318ecbf2e47
Vadim Platonov <vadim@inventi.lt>
2013-11-12 17:05:09 +0200

    Move clojure dependency to dev profile

--- project.clj
+++ project.clj
@@ -1,13 +1,13 @@
 (defproject pocheshiro "0.1.1-SNAPSHOT"
   :description "Clojure wrapper for Apache Shiro"
   :min-lein-version "2.0.0"
-  :dependencies [[org.clojure/clojure "1.5.1"]
-                 [org.apache.shiro/shiro-web "1.2.2"]
+  :dependencies [[org.apache.shiro/shiro-web "1.2.2"]
                  [org.mindrot/jbcrypt "0.3m"]]
   :license {:name "MIT License"
             :url "http://choosealicense.com/licenses/mit/"}
   :url "http://github.com/inventiLT/Pocheshiro"
   :scm {:name "git"
         :url "http://github.com/inventiLT/Pocheshiro"}
-  :profiles {:dev {:dependencies [[compojure "1.1.5"]
+  :profiles {:dev {:dependencies [[org.clojure/clojure "1.5.1"]
+                                  [compojure "1.1.5"]
                                   [javax.servlet/servlet-api "2.5"]]}})
